fc235b6965727819cfa193683143e0860baab157
ysera34 <ycyoon34@gmail.com>
2018-07-05 23:18:33 +0900

    update users model

--- api/models/users.js
+++ api/models/users.js
@@ -8,34 +8,44 @@ export default define('users', {
     autoIncrement: true,
   },
   email: {
-
+    type: DataType.STRING(64),
+    allowNull: false,
   },
   name: {
-
+    type: DataType.STRING(64),
+    allowNull: false,
   },
   gender: {
-
+    type: DataType.STRING(64),
+    allowNull: false,
   },
   phone: {
-
+    type: DataType.STRING(64),
+    allowNull: false,
   },
   birth: {
-
+    type: DataType.DATE,
+    allowNull: false,
   },
   image_path: {
-
+    type: DataType.STRING(255),
+    allowNull: false,
   },
   id_image_path: {
-
+    type: DataType.STRING(255),
+    allowNull: false,
   },
   card_image_path: {
-
+    type: DataType.STRING(255),
+    allowNull: false,
   },
   created_at: {
-
+    type: DataTypes.DATE,
+    allowNull: false,
   },
   updated_at: {
-
+    type: DataTypes.DATE,
+    allowNull: false,
   }
 }, {
   tableName: 'users',
