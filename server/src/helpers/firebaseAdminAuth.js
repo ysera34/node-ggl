0e4f4876d9b8b0f754d1e6ebfab9ef994da8af6b
ysera34 <ycyoon34@gmail.com>
2018-08-12 21:06:03 +0900

    firebase admin auth

---
+++ server/src/helpers/firebaseAdminAuth.js
@@ -0,0 +1,51 @@
+import app from '../app'
+
+export const createCustomToken = (uid) => {
+  app.admin.auth().createCustomToken(uid)
+    .then((customToken) => {
+      console.log('customToken', customToken)
+      return customToken
+    })
+    .catch((error) => {
+      console.error('Error creating custom token:', error)
+      return false
+    })
+}
+
+export const createCustomTokenAsyncAwait = async (uid) => {
+  try {
+    const customToken = await app.admin.auth().createCustomToken(uid)
+    console.log('customToken', customToken)
+    return customToken
+  } catch (error) {
+    console.log('Error creating cusotm token: ', error)
+    return false
+  }
+}
+
+export const verifyIdToken = (idToken) => {
+  app.admin.auth().verifyIdToken(idToken)
+    .then((decodedToken) => {
+      console.log('decodedToken', decodedToken)
+      const uid = decodedToken.uid;
+      console.log('uid', uid)
+      return uid
+    })
+    .catch((error) => {
+      console.error('Error verifying id token:', error)
+      return false
+    })
+}
+
+export const verifyIdTokenAsyncAwait = async (idToken) => {
+  try {
+    const decodedToken = await app.admin.auth().verifyIdToken(idToken)
+    console.log('decodedToken', decodedToken)
+    const uid = decodedToken.uid;
+    console.log('uid', uid)
+    return uid
+  } catch (error) {
+    console.error('Error verifying id token:', error)
+    return false
+  }
+}
