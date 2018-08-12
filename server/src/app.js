02db9fb3a3575eb418720b261c21b90854226741
ysera34 <ycyoon34@gmail.com>
2018-08-12 11:16:32 +0900

    firebase admin sdk

--- server/src/app.js
+++ server/src/app.js
@@ -2,11 +2,22 @@ import express from 'express'
 import router from './routes'
 import bodyParser from 'body-parser'
 
+import * as admin from 'firebase-admin'
+
+import serviceAccount from './configs/firebase-key.json'
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: `https://${serviceAccount.project_id}.firebaseio.com`
+})
+
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/api/v1', router)
+
+app.admin = admin
 
 export default app;
