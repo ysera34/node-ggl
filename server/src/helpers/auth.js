eec80306cc802c2275a5253b1ffe3868cfa622c4
ysera34 <ycyoon34@gmail.com>
2018-08-15 00:46:25 +0900

    json web token

---
+++ server/src/helpers/auth.js
@@ -0,0 +1,16 @@
+import jwt from 'jsonwebtoken'
+import configs from '../configs'
+
+export const signToken = (payload) => {
+  const authToken = jwt.sign(payload, configs.secret, {
+    expiresIn: '1d'
+  })
+  console.log('signToken', authToken)
+  return authToken
+}
+
+export const verifyToken = async (authToken) => {
+  const decoded = await jwt.verify(authToken, configs.secret).catch(error)
+  console.log('decoded', decoded)
+  return decoded
+}
