e65a7120183970e2ea4c19ca8e8f9e928060fbe1
ysera34 <ycyoon34@gmail.com>
2018-08-15 16:57:20 +0900

    add party and application model

---
+++ server/src/database/models/applications.js
@@ -0,0 +1,77 @@
+export default (sequelize, DataTypes) => {
+  const attributes = {
+    id: {
+      type: DataTypes.INTEGER(11),
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING(64),
+      allowNull: false,
+    },
+    site: {
+      type: DataTypes.STRING(64),
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.STRING(64),
+      allowNull: false,
+    },
+    date: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    startAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      field: 'start_at',
+    },
+    endAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      field: 'end_at',
+    },
+    dueDate: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      field: 'due_date',
+    },
+    price: {
+      type: DataTypes.INTEGER(11),
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    limit: {
+      type: DataTypes.INTEGER(11),
+      allowNull: false,
+    },
+    limitMale: {
+      type: DataTypes.INTEGER(11),
+      allowNull: false,
+      field: 'limit_male',
+    },
+    limitFemale: {
+      type: DataTypes.INTEGER(11),
+      allowNull: false,
+      field: 'limit_female',
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      field: 'created_at',
+      defaultValue: 'CURRENT_TIMESTAMP'
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      field: 'updated_at',
+      defaultValue: 'CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'
+    }
+  }
+  const options = {
+    tableName: 'parties',
+  }
+  return sequelize.define('parties', attributes, options)
+}
